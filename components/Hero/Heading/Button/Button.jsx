e74449e9e7dcad58363b9808d75fd0b189f88fe6
Nikita <nikitamaryniuk@gmail.com>
2024-02-12 23:06:03 +0100

    just started doing modal window

--- components/Hero/Heading/Button/Button.jsx
+++ components/Hero/Heading/Button/Button.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Button.module.scss'
 const Button = () => {
-  const handleClick = () => {
-    console.log('button check')
+  const [showModal, SetShowModal] = useState(false)
+
+  const openModal = () => {
+    // Function for form opening
+    SetShowModal(true)
   }
+
+  const closeModal = () => {
+    // Function for form closing
+    showModal(false)
+  }
+
+  const modalWindowLogic = () => {}
+
   return (
     <div className={styles.buttonWrapper}>
-      <a href="#!" onClick={handleClick} className={styles.button}>
+      <a href="#!" onClick={openModal} className={styles.button}>
         Explore more
       </a>
+      {showModal ? (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <span className={styles.close} onClick={closeModal}>
+              &times;
+            </span>
+            {/* Здесь вы можете разместить содержимое вашего модального окна */}
+          </div>
+        </div>
+      ) : null}
     </div>
   )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import Header from '../components/Header/Header.jsx'
 import Heading from '../components/Hero/Heading/Heading.jsx'
 import HeroImg from '../components/Hero/HeroImg/HeroImg.jsx'
 import './App.css'
 import styles from './App.module.scss'
+
 function App() {
   return (
-    <div className={styles.container}>
-      <Header />
-      <div className={styles.hero}>
-        <div className={styles.heroContent}>
-          <Heading />
-          <HeroImg />
+    <BrowserRouter>
+      <div>
+        <div className={styles.container}>
+          <Header />
+          <div className={styles.hero}>
+            <div className={styles.heroContent}>
+              <Heading />
+              <HeroImg />
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </BrowserRouter>
   )
 }
 
