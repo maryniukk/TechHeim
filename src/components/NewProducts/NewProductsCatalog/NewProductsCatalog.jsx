2385943976922535c03e47c15948b7644a34321b
Nikita <nikitamaryniuk@gmail.com>
2024-02-22 12:44:33 +0100

    22.02

--- src/components/NewProducts/NewProductsCatalog/NewProductsCatalog.jsx
+++ src/components/NewProducts/NewProductsCatalog/NewProductsCatalog.jsx
@@ -1,20 +1,22 @@
 import React from 'react'
+import star from '../../../img/Star.png'
+import styles from './NewProductsCatalog.module.scss'
 import unmappedArray from './NpArray'
 const NewProductsCatalog = () => {
-  const mappedArray = unmappedArray.map((array) => (
-    <div>
-      <img src={array.productImg} alt="" />
-      <p>{array.productName}</p>
-      <p>{array.price}</p>
-      <img src={array.starImg} alt="" />
-      <p>{array.rating}</p>
+  const mappedArray = unmappedArray.map((array, index) => (
+    <div key={index}>
+      <div className={styles.box} key={index}>
+        <img className={styles.productImg} src={array.productImg} alt="" />
+        <p className={styles.productName}>{array.productName}</p>
+        <p>{array.price}</p>
+        <div className={styles.flex}>
+          <img className={styles.img} src={star} alt="" />
+          <p>{array.rating}</p>
+        </div>
+      </div>
     </div>
   ))
-  return (
-    <div>
-      <div>{mappedArray}</div>
-    </div>
-  )
+  return <div>{mappedArray}</div>
 }
 
 export default NewProductsCatalog
