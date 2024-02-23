0c3462365c993c34926785d73b8a5f4268ef9937
Nikita <nikitamaryniuk@gmail.com>
2024-02-24 00:19:07 +0100

    24.02

--- src/components/NewProducts/NewProductsCatalog/NewProductsCatalog.jsx
+++ src/components/NewProducts/NewProductsCatalog/NewProductsCatalog.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import star from '../../../img/Star.png'
+import star from '../../../img/Star.png' // Импортируем изображение звездочки
 import styles from './NewProductsCatalog.module.scss'
 import unmappedArray from './NpArray'
+
 const NewProductsCatalog = () => {
   const mappedArray = unmappedArray.map((array, index) => (
-    <div key={index}>
+    <div className={styles.wholeBox} key={index}>
       <div className={styles.box} key={index}>
         <img className={styles.productImg} src={array.productImg} alt="" />
         <p className={styles.productName}>{array.productName}</p>
         <p>{array.price}</p>
-        <div className={styles.flex}>
-          <img className={styles.img} src={star} alt="" />
-          <p>{array.rating}</p>
+        {/* Добавляем изображение звездочки */}
+        <div className={styles.ratingContainer}>
+          <img className={styles.ratingStar} src={star} alt="Rating" />
+          <p className={styles.ratingValue}>{array.rating}</p>
         </div>
       </div>
     </div>
   ))
-  return <div>{mappedArray}</div>
+
+  return <div className={styles.flex}>{mappedArray}</div>
 }
 
 export default NewProductsCatalog
