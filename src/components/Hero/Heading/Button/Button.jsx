442a79503f55234704bc67a39799061cadf6b3f0
Nikita <nikitamaryniuk@gmail.com>
2024-02-25 12:53:13 +0100

    25.02

--- src/components/Hero/Heading/Button/Button.jsx
+++ src/components/Hero/Heading/Button/Button.jsx
@@ -1,24 +1,23 @@
+import { Dialog } from '@headlessui/react'
 import React, { useState } from 'react'
 import styles from './Button.module.scss'
 
 const Button = () => {
-  const [showModal, setShowModal] = useState(false)
-
-  const openModal = () => {
-    // Функция для открытия модального окна
-    setShowModal(true)
-  }
-
-  const closeModal = () => {
-    // Функция для закрытия модального окна
-    setShowModal(false)
-  }
-
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <div className={styles.buttonWrapper}>
-      <a href="#!" onClick={openModal} className={styles.button}>
+      <a onClick={() => setIsOpen(true)} href="#" className={styles.button}>
         Explore more
       </a>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)} className={styles.modalWindow}>
+        <div className={styles.bg}>
+          <Dialog.Panel className={styles.popup}>
+            <Dialog.Title>Название</Dialog.Title>
+            <Dialog.Description>Описание</Dialog.Description>
+            <button onClick={() => setIsOpen(false)}>x</button>
+          </Dialog.Panel>
+        </div>
+      </Dialog>
     </div>
   )
 }
