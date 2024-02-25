d7943813b5aed0ff0ceddef8fe58df7af7f11e0f
Nikita <nikitamaryniuk@gmail.com>
2024-02-25 13:21:42 +0100

    modal started working

--- src/components/Hero/Heading/Button/Button.jsx
+++ src/components/Hero/Heading/Button/Button.jsx
@@ -4,19 +4,23 @@ import styles from './Button.module.scss'
 
 const Button = () => {
   const [isOpen, setIsOpen] = useState(false)
+
   return (
     <div className={styles.buttonWrapper}>
       <a onClick={() => setIsOpen(true)} href="#" className={styles.button}>
         Explore more
       </a>
+
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} className={styles.modalWindow}>
-        <div className={styles.bg}>
-          <Dialog.Panel className={styles.popup}>
-            <Dialog.Title>Название</Dialog.Title>
-            <Dialog.Description>Описание</Dialog.Description>
-            <button onClick={() => setIsOpen(false)}>x</button>
-          </Dialog.Panel>
-        </div>
+        <Dialog.Panel>
+          <button onClick={() => setIsOpen(false)}>x</button>
+          <Dialog.Title>Log in to Tech Heim</Dialog.Title>
+
+          <Dialog.Description>
+            <input type="text" placeholder="Email"></input>
+            <input type="password" placeholder="Password"></input>
+          </Dialog.Description>
+        </Dialog.Panel>
       </Dialog>
     </div>
   )
