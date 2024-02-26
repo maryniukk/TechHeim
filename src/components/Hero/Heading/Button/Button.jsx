388ef224983aa203d402d6b0b1319eeee410800b
Nikita <nikitamaryniuk@gmail.com>
2024-02-26 19:10:36 +0100

    modal styled

--- src/components/Hero/Heading/Button/Button.jsx
+++ src/components/Hero/Heading/Button/Button.jsx
@@ -14,11 +14,15 @@ const Button = () => {
       <Dialog open={isOpen} onClose={() => setIsOpen(false)} className={styles.modalWindow}>
         <Dialog.Panel>
           <button onClick={() => setIsOpen(false)}>x</button>
-          <Dialog.Title>Log in to Tech Heim</Dialog.Title>
+          <Dialog.Title className={styles.title}>Log in to Tech Heim</Dialog.Title>
 
           <Dialog.Description>
-            <input type="text" placeholder="Email"></input>
-            <input type="password" placeholder="Password"></input>
+            <div>
+              <input className={styles.inputMail} type="text" placeholder="Email"></input>
+            </div>
+            <div>
+              <input className={styles.inputPassword} type="password" placeholder="Password"></input>
+            </div>
           </Dialog.Description>
         </Dialog.Panel>
       </Dialog>
--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-import { defineConfig } from 'vite'
-import path from 'path'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: [
-      { find: '@/components', replacement: path.resolve(__dirname, 'src/components') },
-    ],
+    alias: [{ find: '@/components', replacement: path.resolve(__dirname, 'src/components') }],
   },
 })
